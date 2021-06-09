ae1c2db4205013b4d985ed711b510599012bf2ec
Leandro <leandroquiroga9514@gmail.com>
2021-06-09 00:35:16 -0300

    javascript

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,21 @@
 const menu = document.querySelector(".nav-menu");
 const nav = document.querySelector(".list");
 const showMenu = document.querySelector(".menu-bar");
+const close = document.querySelector(".nav-close");
 
 menu.addEventListener('click', () => {
     nav.classList.toggle('show');
     showMenu.style.display = 'block';
-})
+    close.style.display = 'block';
+    close.style.zIndex = '2';
+})  
 
-nav.addEventListener('click', () => {
+nav.addEventListener('click', () => { 
     nav.classList.toggle('show');
     showMenu.style.display = 'none';
 })
+
+close.addEventListener('click', () => {
+    nav.classList.toggle('show');
+    showMenu.style.display = 'none'
+} ) 
